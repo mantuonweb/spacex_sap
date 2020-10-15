91f6bb1ec497824eefbed82d2a93b2f32fd87c11
mantuonweb <mantuonweb@hotmail.com>
2020-10-15 10:03:53 +0530

    Update launches.component.ts

--- src/app/components/launches/launches.component.ts
+++ src/app/components/launches/launches.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
-
+import { fromEvent } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 @Component({
   selector: "spx-launches",
   templateUrl: "./launches.component.html",
@@ -7,8 +8,24 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class LaunchesComponent implements OnInit {
   limitUpper = 15;
+  offsetHeight = 20;
   @Input("launches") launches$: any;
   constructor() {}
+  ngOnInit() {
+    const scroll$ = fromEvent(document, "scroll").pipe(debounceTime(300));
+    const el = document.querySelector("body");
+    scroll$.subscribe(e => {
+      if (this.hasReachedBottom()) {
+        this.limitUpper = Math.min(this.limitUpper + 10, 100);
+      }
+    });
+  }
 
-  ngOnInit() {}
+  hasReachedBottom() {
+    let docEle = document.documentElement;
+    return (
+      Math.ceil(docEle.scrollTop + docEle.clientHeight + this.offsetHeight) >=
+      Math.ceil(docEle.scrollHeight)
+    );
+  }
 }
